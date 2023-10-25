4f0f5fddca862f6387f2dda285c287211a69bcae
HugoEmanuelLC <helc85.info@gmail.com>
2023-10-25 15:52:37 +0200

    exo duo bemovie

--- js/modal.js
+++ js/modal.js
@@ -14,7 +14,6 @@ document.addEventListener('click', (e)=>{
 
 // MODAL ON OFF
 function verifModal( modal) {
-    console.log(modal);
     if (document.querySelector(`.${modal}`).matches('.modal_desactive') == true) {
         document.querySelector(`.${modal}`).className = `modal ${modal} modal_active`
     } else {
@@ -34,19 +33,14 @@ btn_modal_login_on.addEventListener('click', ()=> verifModal('modal_register_log
 // REGISTER LOGIN
 document.addEventListener('click', (e)=>{
     e.stopImmediatePropagation()
-    console.log(e.target);
-    console.log(choice);
 
     if (e.target.className == 'sig') {
-        console.log('sig');
         register_login_item.innerHTML = template_register_item
         
     }else if(e.target.className == 'log') {
-        console.log('log');
         register_login_item.innerHTML = template_login_item
 
     }else if( e.target.innerHTML == choice.innerHTML){
-        console.log('choice');
         register_login_item.innerHTML = template_register_item
     }
 })
